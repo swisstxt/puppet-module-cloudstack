c145f44a77c81376b011411bdb6d610ab662e468
niwo <nik.wolfgramm@swisstxt.ch>
2012-11-01 18:33:21 +0100

    tabs to spaces

--- lib/puppet/provider/cloudstack_loadbalancer/cloudstack.rb
+++ lib/puppet/provider/cloudstack_loadbalancer/cloudstack.rb
@@ -2,77 +2,77 @@ require File.join(File.dirname(__FILE__), '../../../util/cloudstack_client')
 
 Puppet::Type.type(:cloudstack_loadbalancer).provide(:cloudstack) do
 
-	desc "Provider for the Cloudstack load balancer."
+  desc "Provider for the Cloudstack load balancer."
 
-	def load_rules
-		loadbalancer_rules = []
-		projects = api.list_projects
+  def load_rules
+    loadbalancer_rules = []
+    projects = api.list_projects
 
-		projects.each do |project|
-			params = {
+    projects.each do |project|
+      params = {
         'command' => 'listLoadBalancerRules',
-				'projectid' => project['id']
+        'projectid' => project['id']
       }
       json = api.send_request(params)
       loadbalancer_rules += json['loadbalancerrule']
-		end
-		return loadbalancer_rules
-	end
+    end
+    return loadbalancer_rules
+  end
 
-	def create
-		project_id = nil
-		projects = api.list_projects
+  def create
+    project_id = nil
+    projects = api.list_projects
     project = projects.find {|project| project['name'] == @resource[:projectname] }
 
-		params = {
-        'command' => 'listPublicIpAddresses',
-        'ipaddress' => @resource[:vip],
-				'projectid' => project['id']
+    params = {
+      'command' => 'listPublicIpAddresses',
+      'ipaddress' => @resource[:vip],
+      'projectid' => project['id']
     }
 
-		json = api.send_request(params)
-		publicaddressid = json['publicipaddress'].first['id']
+    json = api.send_request(params)
+    publicaddressid = json['publicipaddress'].first['id']
 
-		params = {
-        'command' => 'createLoadBalancerRule',
-				'privateport' => @resource[:privateport],
-				'publicport' => @resource[:publicport],
-				'algorithm' => @resource[:algorithm],
-        'publicipid' => publicaddressid,
-				'name' => @resource[:name]
+    params = {
+      'command' => 'createLoadBalancerRule',
+      'privateport' => @resource[:privateport],
+      'publicport' => @resource[:publicport],
+      'algorithm' => @resource[:algorithm],
+      'publicipid' => publicaddressid,
+      'name' => @resource[:name]
     }
     api.send_request(params)
-		true
-	end
+    true
+  end
 
-	def destroy
+  def destroy
     loadbalancer_rules = load_rules
     rule = loadbalancer_rules.find {|rule| rule['name'] == @resource[:name] } || false
     if rule
-  		params = {
-      	  'command' => 'deleteLoadBalancerRule',
-					'id' => rule['id']
-			}
-			api.send_request(params)
-		end
-		true
-	end
+      params = {
+        'command' => 'deleteLoadBalancerRule',
+        'id' => rule['id']
+      }
+      api.send_request(params)
+    end
+    true
+  end
 
-	def exists?
-		loadbalancer_rules = load_rules
-		loadbalancer_rules.each do |rule|
-			return true if rule['name'] == @resource[:name]
-		end
-		return false
-	end
+  def exists?
+    loadbalancer_rules = load_rules
+    loadbalancer_rules.each do |rule|
+      return true if rule['name'] == @resource[:name]
+    end
+    return false
+  end
 
-	private
+  private
 
   def api
-		api = CloudstackClient::Connection.new(
-        @resource[:cloudstack_url],
-        @resource[:cloudstack_api_key],
-        @resource[:cloudstack_secret_key]
+    api = CloudstackClient::Connection.new(
+      @resource[:cloudstack_url],
+      @resource[:cloudstack_api_key],
+      @resource[:cloudstack_secret_key]
     )
-	end
+  end
 end
