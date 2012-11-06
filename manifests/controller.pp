0830a47f75cfba484ecf0bffdcc506e320ca3a4c
Andreas Zuber <zuber@puzzle.ch>
2012-11-06 13:25:15 +0100

    add project variable to controller.yaml

--- manifests/controller.pp
+++ manifests/controller.pp
@@ -11,15 +11,18 @@
 # *Mandatory* The Cloudstack API Key
 # [secret_key]
 # *Mandatory* The Cloudstack Secret Key
+# [project]
+# *Mandatory* The Cloudstack Project for the controller 
 #
 class cloudstack::controller(
   $url,
   $api_key,
   $secret_key,
+  $project,
 ){
 
     file{'/etc/cloudstack.yaml':
-      content => "url:  '$url'\napi_key:  '$api_key'\nsecret_key:  '$secret_key'\n",
+      content => "url:  '$url'\napi_key:  '$api_key'\nsecret_key:  '$secret_key'\nproject:  '$project'\n",
       owner => root, group => 0, mode => 0400;
     }
 
